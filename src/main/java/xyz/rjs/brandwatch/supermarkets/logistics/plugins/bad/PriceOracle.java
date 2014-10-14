1183a445386691e0ed0846f948640c794983f492
Matthew Franglen <matthew@franglen.org>
2014-10-15 00:44:32 +0100

    Create common superclass of Price and Sale Oracle
    They do the exact same thing

--- src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/bad/PriceOracle.java
+++ src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/bad/PriceOracle.java
@@ -1,12 +1,7 @@
 package xyz.rjs.brandwatch.supermarkets.logistics.plugins.bad;
 
-import java.util.Random;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import xyz.rjs.brandwatch.supermarkets.logistics.plugins.AbstractPlugin;
 import xyz.rjs.brandwatch.supermarkets.model.events.PriceList;
 
 import com.google.common.eventbus.Subscribe;
@@ -17,54 +12,30 @@ import com.google.common.eventbus.Subscribe;
  * @author matthew
  */
 @Component
-public class PriceOracle extends AbstractPlugin {
-
-	private static final Logger logger = LoggerFactory.getLogger(PriceOracle.class);
+public class PriceOracle extends OracleWrapper {
 
 	private static final int PRICE_BOUND = 3;
-
-	private final Oracle oracle;
 
 	private int price;
 
 	public PriceOracle() {
-		oracle = new Oracle();
+		super(PRICE_BOUND);
 		price = 1;
-	}
-
-	public boolean isFixed() {
-		return oracle.size() == 1;
-	}
-
-	public Random getRandom() {
-		return oracle.getRandom();
 	}
 
 	@Subscribe
 	public void priceListListener(PriceList list) {
 		// Get the Random.nextInt result by adding one
 		final int value = 1 + list.getCurrentPrice() - price;
-		final long originalSize = oracle.size();
 
 		if (price == 1 && value == 1) {
 			// When the price is 1 the price cannot go down. This means that the
 			// value of 1 is ambiguous as it could be a 0 that was fixed.
-			oracle.calledNextInt(r -> r.nextInt(3) < 2, PRICE_BOUND);
+			calledNextInt(r -> r.nextInt(3) < 2);
 		}
 		else {
-			oracle.calledNextInt(value, PRICE_BOUND);
+			calledNextInt(value);
 		}
 		price = list.getCurrentPrice();
-
-		if (originalSize > 1) {
-			final long size = oracle.size();
-
-			if (size == 1) {
-				logger.info("SaleOracle has fixated!");
-			}
-			else {
-				logger.info(String.format("SaleOracle (%d -> %d)", originalSize, size));
-			}
-		}
 	}
 }
--- src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/bad/SaleOracle.java
+++ src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/bad/SaleOracle.java
@@ -1,12 +1,7 @@
 package xyz.rjs.brandwatch.supermarkets.logistics.plugins.bad;
 
-import java.util.Random;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import xyz.rjs.brandwatch.supermarkets.logistics.plugins.AbstractPlugin;
 import xyz.rjs.brandwatch.supermarkets.model.events.Sale;
 
 import com.google.common.eventbus.Subscribe;
@@ -17,42 +12,19 @@ import com.google.common.eventbus.Subscribe;
  * @author matthew
  */
 @Component
-public class SaleOracle extends AbstractPlugin {
-
-	private static final Logger logger = LoggerFactory.getLogger(SaleOracle.class);
+public class SaleOracle extends OracleWrapper {
 
 	private static final int SALE_BOUND = 6;
 
-	private final Oracle oracle;
-
 	public SaleOracle() {
-		oracle = new Oracle();
-	}
-
-	public boolean isFixed() {
-		return oracle.size() == 1;
-	}
-
-	public Random getRandom() {
-		return oracle.getRandom();
+		super(SALE_BOUND);
 	}
 
 	@Subscribe
 	public void saleListener(Sale sale) {
 		// Get the Random.nextInt result by subtracting one
 		final int value = sale.getAmountSold() - 1;
-		final long originalSize = oracle.size();
 
-		oracle.calledNextInt(value, SALE_BOUND);
-		if (originalSize > 1) {
-			final long size = oracle.size();
-
-			if (size == 1) {
-				logger.info("SaleOracle has fixated!");
-			}
-			else {
-				logger.info(String.format("SaleOracle (%d -> %d)", originalSize, size));
-			}
-		}
+		calledNextInt(value);
 	}
 }
